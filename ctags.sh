67182b73da5a5fcfdea547fac3f541b9cb9cf9e2
eddy <wojia518@126.com>
2017-11-09 10:20:09 +0800

    Delete ctags.sh

--- ctags.sh
+++
@@ -1,7 +0,0 @@
-if [ $# -eq 0 ]; then
-  echo "Usage ctags.sh <full_path>"
-  exit 0
-else
-  echo "ctags find directory is "$1
-fi
-ctags -R --languages=c++ --langmap=c++:+.inl -h +.inl --c++-kinds=+px --fields=+aiKSz --extra=+q --exclude=lex.yy.cc --exclude=copy_lex.yy.cc $1
